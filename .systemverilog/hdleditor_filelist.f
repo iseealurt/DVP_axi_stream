041f36cb3e0290d27da2df0b5ec91c1165329406
iseealurt <sam_max@foxmail.com>
2026-08-29 20:34:05 +0800

    new file:   bench/tb_DVP2axi_stream_v_1_0.sv

--- .systemverilog/hdleditor_filelist.f
+++ .systemverilog/hdleditor_filelist.f
@@ -1,7 +1,8 @@
 # 由 SystemVerilog HDL 编辑器生成，请勿手动编辑。
 # 来源：工作区通配扫描
-# 已索引 HDL 文件数：3
-# 编译文件列表文件数：3
+# 已索引 HDL 文件数：4
+# 编译文件列表文件数：4
 RTL/DVP2axi_stream.v
+bench/tb_DVP2axi_stream_v_1_0.sv
 bench/tb_DVP2axi_stream.sv
 bench/AXI4-lite.sv
---
+++ bench/tb_DVP2axi_stream_v_1_0.sv
@@ -0,0 +1,5 @@
+`timescale 1ns/1ps
+module tb_DVP2axi_stream_v_1_0;
+  `include "AXI4-lite.sv"
+  
+endmodule
