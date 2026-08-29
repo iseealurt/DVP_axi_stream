64e9882f51b84ddef937ff80f56017b18a6c61a6
iseealurt <sam_max@foxmail.com>
2026-08-29 20:44:21 +0800

    modified:   bench/tb_DVP2axi_stream_v_1_0.sv

--- bench/tb_DVP2axi_stream_v_1_0.sv
+++ bench/tb_DVP2axi_stream_v_1_0.sv
@@ -1,5 +1,23 @@
 `timescale 1ns/1ps
+`include "AXI4-lite.sv"
+
 module tb_DVP2axi_stream_v_1_0;
-  `include "AXI4-lite.sv"
+  // ------------------------------ Parameters ------------------------------
+  parameter DVP_DWIDTH = 8 ;
+  parameter PIX_WIDTH = 16 ;
+  parameter AXI_ID_WIDTH = 4 ;
+  parameter AXI_STREAM_TID = 1 ;
+  parameter AXI_STREAM_DWIDTH = 256 ;
+  localparam AXI_STREAM_STRB_WIDTH = AXI_STREAM_DWIDTH/8 ;
+  parameter AXI_STREAM_USER_WIDTH = 4 ;
+  parameter AXI_STREAM_TDEST_WIDTH = 4 ;
+  parameter AXI_LITE_DWIDTH = 32 ;
+  localparam AXI_LITE_STRB_WIDTH = AXI_LITE_DWIDTH/8 ;
+  parameter AXI_LITE_AWIDTH = 32 ;
+  parameter AXI_LITE_BASE_ADDR_OFFSET = 32'h0000_0000 ;
+
+  // ------------------------------ Imported packages ------------------------------
+  import axil_test_pkg::*;
+  // ------------------------------ DUT ------------------------------
   
 endmodule
